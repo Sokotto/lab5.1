817dfde2e4b0ce59a9d416f22a1b194e6dbb23cc
Компьютер <@DESKTOP-RDMTMQN>
2024-12-01 15:24:42 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите строку: ");
+string input = Console.ReadLine();
+string transformedString = TransformString(input);
+Console.WriteLine("Преобразованная строка: " + transformedString);
+static string TransformString(string input)
+{
+    char[] result = new char[input.Length * 2];
+    int index = 0;
+
+    foreach (char c in input)
+    {
+        if (c != '*')
+        {
+            result[index++] = c;
+            result[index++] = c;
+        }
+    }
+    return new string(result, 0, index);
+}
